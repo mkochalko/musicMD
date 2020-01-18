0669d470c0a06f94c0de89fd80df77a59d51cf8f
Jonathan Lai <jonathanlai95@gmail.com>
2020-01-18 12:42:56 -0800

    Add User Auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jonjonlai:bbba5yr7@cluster0-xr0ct.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'gofuckyourselfbuddy'
+    //Make sure this is your own unique string
+}
