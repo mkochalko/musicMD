c81c420b6917f7f01dacb7f631a2abe65cfd001e
Jonathan Lai <jonathanlai95@gmail.com>
2020-01-21 11:17:40 -0800

    Key Change

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Key from "./keys"
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -17,7 +18,7 @@ class SimpleMap extends Component {
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyCoJ6ho5vY8s9WX13q025r3tn4SeHfuLPE" }}
+                    bootstrapURLKeys={{ key: Key }}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                 >
