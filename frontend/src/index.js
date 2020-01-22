01afcec08c3d9437e1a1bbc8b9a3d2edd620f234
mkochalko <matthew.kochalko@gmail.com>
2020-01-22 13:10:05 -0800

    Test Keys

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,7 @@ import jwt_decode from 'jwt-decode';
 import { setAuthToken, login } from './util/session_api_util';
 import { logout } from './actions/session_actions';
 import axios from 'axios';
-<<<<<<< HEAD
 import { getMetroIdByClick, getMetroIdByText } from './actions/songkick_actions';
-=======
-import { fetchEvents } from './actions/event_actions';
-import { getMetroIdByClick, getMetroIdByText } from './actions/ticketmaster_actions';
->>>>>>> master
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -42,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     window.logout = logout;
     // window.dispatch = dispatch;
     window.getState = store.getState;
-    window.fetchEvents = fetchEvents;
+    // window.fetchEvents = fetchEvents;
     window.getMetroIdByClick = getMetroIdByClick;
     window.getMetroIdByText = getMetroIdByText;
 
