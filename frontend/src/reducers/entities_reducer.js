c310ec1753d3b483926c882dab4a5bc42ef047fe
mkochalko <matthew.kochalko@gmail.com>
2020-01-22 13:15:17 -0800

    Fix merge issue

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,20 +1,10 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
 import songkick from './songkick_reducer';
-=======
-import ticketmaster from './ticketmaster_reducer';
-import events from './events_reducer';
->>>>>>> master
 import user from './users_reducer';
 
 const RootReducer = combineReducers({
     user,
-<<<<<<< HEAD
     songkick
-=======
-    ticketmaster,
-    events
->>>>>>> master
 });
 
 export default RootReducer;
