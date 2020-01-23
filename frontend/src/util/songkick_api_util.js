a0d9f4514f2b45262750384eda9d3b3d9245f5a0
kit <ankitspatel1145@gmail.com>
2020-01-23 14:40:47 -0800

    change SK to TM

--- frontend/src/util/songkick_api_util.js
+++ frontend/src/util/songkick_api_util.js
@@ -18,7 +18,8 @@ export const getEvents = (metroId) => {
 export const getMetroIdByClick = (latlng) => {
     return (
         $.ajax({
-            url: `https://api.songkick.com/api/3.0/search/locations.json?location=geo:${latlng}&apikey=${window.SKKey}`,
+            url: `https://app.ticketmaster.com/discovery/v2/events?apikey=${window.SKKey}&latlong=${latlng}&radius=20&unit=miles&locale=*&startDateTime=2020-01-23T13:44:00Z&endDateTime=2020-03-31T13:43:00Z&sort=relevance,desc&segmentId=KZFzniwnSyZfZ7v7nJ`,
+            // url: `https://api.songkick.com/api/3.0/search/locations.json?location=geo:${latlng}&apikey=${window.SKKey}`,
             method: 'GET',
             async: false,
             dataType: "json"
