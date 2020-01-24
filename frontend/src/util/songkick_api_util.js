aa50422bc91a22f19d184e1c6a0c511226dcf94b
kit <ankitspatel1145@gmail.com>
2020-01-23 16:30:47 -0800

    change sk to TM

--- frontend/src/util/songkick_api_util.js
+++ frontend/src/util/songkick_api_util.js
@@ -16,26 +16,35 @@ export const getEvents = (metroId) => {
 }
 
 export const getMetroIdByClick = (latlng) => {
-    return (
-        $.ajax({
-            url: `https://app.ticketmaster.com/discovery/v2/events?apikey=${window.SKKey}&latlong=${latlng}&radius=20&unit=miles&locale=*&startDateTime=2020-01-23T13:44:00Z&endDateTime=2020-03-31T13:43:00Z&sort=relevance,desc&segmentId=KZFzniwnSyZfZ7v7nJ`,
-            // url: `https://api.songkick.com/api/3.0/search/locations.json?location=geo:${latlng}&apikey=${window.SKKey}`,
-            method: 'GET',
-            async: false,
-            dataType: "json"
-        })
-    )
+    let today = new Date();
+    var d = (today.getDate() < 10 ? "0" : "") + today.getDate();
+    var m = (today.getMonth() + 1 < 10 ? "0" : "") + (today.getMonth() + 1);
+    var y = today.getFullYear();
+    today = y + "-" + m + "-" + d;
+
+    let later = new Date();
+    later.setMonth(later.getMonth() + 3);
+    var d = (later.getDate() < 10 ? "0" : "") + later.getDate();
+    var m = (later.getMonth() + 1 < 10 ? "0" : "") + (later.getMonth() + 1);
+    var y = later.getFullYear();
+
+    later = y + "-" + m + "-" + d;
+    return $.ajax({
+      url: `https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events?apikey=${window.SKKey}&latlong=34.0522342,-118.2436849&radius=20&unit=miles&locale=*&startDateTime=${today}T15:28:00Z&endDateTime=${later}T15:28:00Z&size=50&page=1&sort=relevance,desc&segmentId=KZFzniwnSyZfZ7v7nJ`,
+      //   url: `https://api.songkick.com/api/3.0/search/locations.json?location=geo:${latlng}&apikey=${window.SKKey}`,
+      method: "GET",
+      async: false,
+      dataType: "json"
+    });
 }
 
 
 
-export const getMetroIdByText = (text) => {
-    return (
-        $.ajax({
-            url: `https://api.songkick.com/api/3.0/search/locations.json?query=${text}&apikey=${window.SKKey}`,
-            method: 'GET',
-            async: false,
-            dataType: "json"
-        })
-    )
+export const getMetroIdByText = (city) => {
+    return $.ajax({
+      url: `https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events?apikey=${window.SKKey}&radius=20&unit=miles&locale=*&startDateTime=2020-01-23T16:26:00Z&endDateTime=2020-04-23T16:26:00Z&size=20&sort=relevance,desc&city=${city}&segmentId=KZFzniwnSyZfZ7v7nJ`,
+      method: "GET",
+      async: false,
+      dataType: "json"
+    });
 }
