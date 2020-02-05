d55071eeb3d9d943ce1a5cb66dbc403d1d72f277
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-02-04 16:35:51 -0800

    polish front end design and style

--- frontend/src/components/user/user_splash.js
+++ frontend/src/components/user/user_splash.js
@@ -20,7 +20,7 @@ class UserSplash extends React.Component {
         return (
             <div className={navClasses.wrapper}>
                 <NavBarContainer className={navClasses.navContainer}></NavBarContainer>
-                <div className={navClasses.userSplash}>
+                <div className={classes.userSplash}>
                     <div className={classes.greetingDiv}>
                         <p className={classes.userGreeting}>Hey {this.props.currentUser.username[0].toUpperCase() + this.props.currentUser.username.slice(1)}</p>
                         <p className={classes.userMessage}>Your prescriptions:</p>
