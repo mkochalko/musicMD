c8652884f295d4eea76238dcef8fc3d40c6280fb
mkochalko <matthew.kochalko@gmail.com>
2020-02-13 18:32:56 -0800

    Refactor bugs

--- frontend/src/components/Library/library_index_item.js
+++ frontend/src/components/Library/library_index_item.js
@@ -18,7 +18,7 @@ class LibraryIndexItem extends React.Component {
         return (
             <li>
                 <label>
-                    <iframe className={classes.deezerTrack} scrolling="no" frameBorder="0" allowtransparency="true" src={`https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=300&height=60&color=ff0000&layout=dark&size=medium&type=tracks&id=${this.props.song}&app_id=1`} width="300" height="60"></iframe>
+                    <iframe className={classes.deezerTrack} scrolling="no" frameBorder="0" allowtransparency="true" src={`https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=500&height=92&color=ff0000&layout=dark&size=medium&type=tracks&id=${this.props.song}&app_id=1`} width="500" height="92"></iframe>
                 </label>
                 <button id={this.props.song} onClick={this.deleteSong}>Remove Song</button>
             </li>
