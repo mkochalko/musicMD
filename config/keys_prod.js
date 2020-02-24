0b56ec41698ce92290e94992ec6314a6dff7fb8e
mkochalko <matthew.kochalko@gmail.com>
2020-02-24 13:58:45 -0800

    Setup production vars

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
     SKKey: process.env.SONG_KICK_KEY,
-    SLKey: process.env.SETLIST_KEY
+    SLKey: process.env.SETLIST_KEY,
+    MapsKey: process.env.MAPS_API_KEY
 }
